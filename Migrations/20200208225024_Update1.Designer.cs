06d925926cb4706bd56f7e86c24583f8b5713eb6
3lpsy <elpsy@protonmail.com>
2020-02-08 16:51:54 -0600

    Update migrations with AgentType "Development" and merged master updates

--- Migrations/20200208225024_Update1.Designer.cs
+++ Migrations/20200208225024_Update1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Faction.Common.Backend.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Faction.Common.Migrations
 {
     [DbContext(typeof(FactionDbContext))]
-    partial class FactionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200208225024_Update1")]
+    partial class Update1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
