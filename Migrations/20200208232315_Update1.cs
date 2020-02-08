2b4b71fe0344089c482498d5fc0fc8f732e63a4a
3lpsy <elpsy@protonmail.com>
2020-02-08 17:23:48 -0600

    Add migration to update Development column for AgentType

---
+++ Migrations/20200208232315_Update1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Faction.Common.Migrations
+{
+    public partial class Update1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Development",
+                table: "AgentType",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Development",
+                table: "AgentType");
+        }
+    }
+}
